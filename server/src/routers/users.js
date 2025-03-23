39759b6ca210012f70199ac28da61a55d81dcf94
artndev <97910780+artndev@users.noreply.github.com>
2025-03-23 23:42:21 +0300

    .

--- server/src/routers/users.js
+++ server/src/routers/users.js
@@ -11,5 +11,26 @@ router.post('/login', middlewares.isNotLogged, await usersController.Login)
 
 router.post('/logout', middlewares.isLogged, usersController.Logout)
 
+router.get("/is_logged/state", middlewares.isLogged, (req, res) => {
+    const user_data = JSON.parse(req.cookies.user_data)
+
+    res.status(200).json({
+        message: "You are authrorized",
+        answer: {
+            user_id: user_data.user_id,
+            username: user_data.username
+        }
+    })
+})
+
+router.get("/is_admin/state", middlewares.isLogged, middlewares.isAdmin, (req, res) => {
+    res.status(200).json({
+        message: "You are admin",
+        answer: true
+    })
+})
+
+
+
 
 export default router
