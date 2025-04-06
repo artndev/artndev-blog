9741010e2c897f71c780101019a1e52251b2b0ae
artndev <97910780+artndev@users.noreply.github.com>
2025-04-06 11:42:00 +0300

    .

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import config from './config.json'
 
 const instance = axios.create({
-  baseURL: config.REST_API_URL,
+  baseURL: config.BACKEND_URL,
   withCredentials: true,
 })
 
