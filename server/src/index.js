4b14aae10c9255777b5d2451580192a032a5f01f
artndev <97910780+artndev@users.noreply.github.com>
2025-04-06 15:01:30 +0300

    .

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,13 @@ app.use(
   })
 )
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*'])
+  res.append('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+  res.append('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.use(express.json())
 app.use(cookieParser())
 
