6e1fc49d9503ca59a83a8bef725d0a2fefdf48d4
artndev <97910780+artndev@users.noreply.github.com>
2025-04-06 13:29:48 +0300

    .

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ import config from './config.json' with { type: 'json' }
 const app = express()
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: config.FRONTEND_URL,
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
   })
